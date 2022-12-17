32964b7586fb4506a3fde2a17f29df9f90819f62
tanmay261 <tanmaytanu.10@gmail.com>
2022-12-17 19:53:21 +0530

    CRUD for Prodcut

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,3 +1,86 @@
-exports.getAllProducts=(req,res)=>{
-    res.status(200).json({message:"Router is working fine"})
+const Product=require("../models/productModel");
+
+//Creating the four CRUD operations
+
+//Create Product => An Admin only Function
+exports.createProduct=async(req,res,next)=>{
+    const product=await Product.create(req.body);
+
+    res.status(201).json({
+        success:true,
+        product
+    })
+}
+
+//Get all products OR Read Products
+exports.getAllProducts=async(req,res)=>{
+    const products = await Product.find();
+
+    res.status(201).json({
+        success:true,
+        products
+    })
+ 
+}
+
+//Get a single product detail
+exports.getProductDetails= async(req,res,next)=>{
+    const product=await Product.findById(req.params.id);
+
+    if(!product)
+    {
+        res.status(500).json({
+            success:false,
+            message:"Product not found"
+        })
+    }
+
+    res.status(200).json({
+        sucesss:true,
+        product
+    })
+}
+
+//Update Product => Admin only Function
+exports.updateProduct=async (req,res,next)=>{
+
+    let product=await Product.findById(req.params.id);
+
+    if(!product){
+        return res.status(500).json({
+            success:false,
+            message:"Product not found"
+        })
+            
+        
+    }
+    product=await Product.findByIdAndUpdate(req.params.id,req.body,{
+new:true,
+runValidators:true,
+useFindAndModify:false
+    });
+
+    res.status(200).json({
+        success:true,
+        product
+    })
+}
+
+//Delete Product => Admin Only Functon
+exports.deleteProduct=async(req,res,next)=>{
+    let product = await Product.findById(req.params.id);
+
+    if(!product){
+        return res.status(500).json({
+            success:false,
+            message:"Product not found"
+        })
+}
+
+await product.remove();
+
+res.status(200).json({
+    success:true,
+    message:"Product Removed successfully"
+})
 }
--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,8 +1,10 @@
 const express=require("express");
-const { getAllProducts } = require("../controllers/productController");
+const { getAllProducts, createProduct, updateProduct, deleteProduct, getProductDetails } = require("../controllers/productController");
 
 const router =express.Router();
 
+//Routes or URLs to call 
 router.route("/products").get(getAllProducts);
-
+router.route("/product/new").post(createProduct);
+router.route("/product/:id").put(updateProduct).delete(deleteProduct).get(getProductDetails);
 module.exports=router
