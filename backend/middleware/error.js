61fb0b186b56222403f731099ea113a441f57fff
Tanmay <tanmaytanu.10@gmail.com>
2023-01-17 16:57:03 +0530

    MongoDB ID error

--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -4,6 +4,12 @@ module.exports=(err,req,res,next)=>{
 
     err.statusCode= err.statusCode || 500;
     err.message = err.message || "Internal Server Error";
+    
+    // Wrong MOngoDB ID error
+    if(err.name==="CaseError"){
+        const message=`Resource not found Invalid : ${err.path}`;
+        err=new ErrorHandler(message,400);
+    }
     res.status(err.statusCode).json({
         
         success:false,
